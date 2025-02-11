6fa9dd9685c49727c7e8834ca6e4cfce41a6d055
paul <paulripault0@gmail.com>
2025-02-11 16:57:48 +0900

    [INIT] application template

---
+++ android/app/src/main/kotlin/com/featherPeak/radio_pop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.featherPeak.radio_pop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RadioPop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Vierge'),
+      ),
+      body: Center(
+        child: Text('Ceci est une page vide.'),
+      ),
+    );
+  }
+}
